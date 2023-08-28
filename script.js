5fa479d9f051fa42001f17fc35085ce339b439b6
Maximilian-andrew <nocopyrightsoundssd@gmail.com>
2023-08-29 00:11:52 +0200

    5

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 document.getElementById("login-form").addEventListener("submit", function(event) {
     event.preventDefault();
     
-    var username = document.getElementById("username").value.toLowerCase(); // Benutzernamen in Kleinbuchstaben umwandeln
+    var identifier = document.getElementById("identifier").value.toLowerCase();
     var password = document.getElementById("password").value;
     
-    if (username === "max" && password === "max") {
-        document.getElementById("success-message").style.display = "block"; // Zeige Erfolgsmeldung an
+    var users = {
+        "admin": "admin",
+        "max": "Max08062008",
+        "exampleuser": "examplepassword"
+        // Weitere Benutzernamen und Passwörter hier hinzufügen
+    };
+    
+    if (users.hasOwnProperty(identifier) && users[identifier] === password) {
+        document.getElementById("success-message").style.display = "block";
         setTimeout(function() {
             window.location.href = "Dro-down Menu/index.html";
-        }, 1000); // Nach 1 Sekunde zur Dashboard-Seite weiterleiten
+        }, 1000);
     } else {
         alert("Fehlerhafte Anmeldeinformationen");
     }
