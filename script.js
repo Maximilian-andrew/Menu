311f0687bc73e2401b7ed246eb8c3d5b1eb6e767
Maximilian-andrew <nocopyrightsoundssd@gmail.com>
2023-08-28 23:51:06 +0200

    4

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     if (username === "max" && password === "max") {
         document.getElementById("success-message").style.display = "block"; // Zeige Erfolgsmeldung an
         setTimeout(function() {
-            window.location.href = "dashboard.html";
+            window.location.href = "Dro-down Menu/index.html";
         }, 1000); // Nach 1 Sekunde zur Dashboard-Seite weiterleiten
     } else {
         alert("Fehlerhafte Anmeldeinformationen");
